0dfc59022e0d0410e87212e8805a192964f0417e
nyovelt <nyovelt@outlook.com>
2021-09-23 12:35:57 +0800

    init

---
+++ Native-Modem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Native_Modem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
