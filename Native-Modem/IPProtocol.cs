05cd76e905085f9b63eac0cbf2cd41174731ec79
nyovelt <nyovelt@outlook.com>
2021-11-29 03:13:15 +0800

    Update IPProtocol.cs

--- Native-Modem/IPProtocol.cs
+++ Native-Modem/IPProtocol.cs
@@ -26,6 +26,8 @@ namespace Native_Modem
         readonly FullDuplexModem modem;
         public IPProtocal()
         {
+            getInterface(); // 获得 ip 配置
+
 
         }
 
@@ -39,6 +41,34 @@ namespace Native_Modem
             Console.Write(File.ReadAllText(configFile));
             var myconfig = deserializer.Deserialize<Interface>(File.ReadAllText(configFile));
             AthernetInterface = myconfig; // problems here
+        }
+
+        public void startFullDuplexModem()
+        {
+            // 获得配置目录 
+            Console.Write("Please enter work folder: ");
+            string workFolder;
+            while (true)
+            {
+                workFolder = Console.ReadLine();
+                if (Directory.Exists(workFolder))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("Folder doesn't exist. Retry: ");
+                }
+            }
+            Protocol protocol = Protocol.ReadFromFile(Path.Combine(workFolder, "protocol.conf"));
+            //Console.WriteLine("Please enter your MAC address (in decimal): ");
+            var rand = new Random();
+            byte address = (byte)rand.Next(255);  // 1.广播，这样只要保证两个 mac 不一样就行了 2. 建立 mac - ip 表
+           
+            //while (!byte.TryParse(Console.ReadLine(), out address))
+            //{
+            //    Console.WriteLine("MAC address invalid. It should be an integer in [0, 255]. Please try again: ");
+            //}
         }
 
 
