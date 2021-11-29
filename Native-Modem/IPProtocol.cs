6a7073ded1492c73abbd067c78d2abcf8a9a1456
nyovelt <nyovelt@outlook.com>
2021-11-29 23:18:16 +0800

    Update IPProtocol.cs

--- Native-Modem/IPProtocol.cs
+++ Native-Modem/IPProtocol.cs
@@ -2,8 +2,11 @@
 #define THROW_PHASE_ERROR
 
 
+using NetTools;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using YamlDotNet.Serialization.NamingConventions;
 
 namespace Native_Modem
@@ -31,6 +34,17 @@ namespace Native_Modem
             shell();
         }
 
+        enum Operation
+        {
+            UDPing
+        }
+
+        static readonly Dictionary<Operation, string[]> ARGUMENTS = new Dictionary<Operation, string[]>(
+    new KeyValuePair<Operation, string[]>[]
+    {
+                new KeyValuePair<Operation, string[]>(Operation.UDPing, new string[1] { "destination" }),
+    });
+
         ~IPProtocal()
         {
             modem.Dispose();
@@ -39,7 +53,89 @@ namespace Native_Modem
         public void shell()
         {
             bool quit = false;
-            // 寻找一个更好的交互式内建命令框架
+            // 寻找一个更好的交互式内建命令框架 // 失败
+            while (!quit)
+            {
+                Console.Write(">");
+                string[] args = Console.ReadLine().Split(' ');
+                if (args.Length == 0)
+                    continue;
+
+                if (!Enum.TryParse(args[0], true, out Operation op))
+                {
+                    Console.WriteLine("Invalid operation. Type 'help' to show help.");
+                    continue;
+                }
+
+                if (args.Length != ARGUMENTS[op].Length + 1)
+                {
+                    Console.WriteLine("Argument count error. Type 'help' to show help.");
+                    continue;
+                }
+
+                switch (op)
+                {
+                    case Operation.UDPing:
+                        var pingDest = args[1];
+                        if (!IsValidIP(pingDest))
+                        {
+
+                        }
+                        else
+                        {
+                            UDPing(pingDest);
+                        }
+                        break;
+                }
+            }
+        }
+
+        public bool IsValidIP(string ip)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(ip, "[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}"))
+            {
+                string[] ips = ip.Split('.');
+                if (ips.Length == 4 || ips.Length == 6)
+                {
+                    if (System.Int32.Parse(ips[0]) < 256 && System.Int32.Parse(ips[1]) < 256 & System.Int32.Parse(ips[2]) < 256 & System.Int32.Parse(ips[3]) < 256)
+                        return true;
+                    else
+                    {
+                        Console.WriteLine("IP invalid"); return false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("IP invalid"); return false;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("IP invalid"); return false;
+            }
+        }
+
+        public bool IsAthernetSubnet(string ipAddress)
+        {
+            if (!IsValidIP(ipAddress))
+                return false;
+            var rangeC = IPAddressRange.Parse(AthernetInterface.addresses);
+
+
+            return rangeC.Contains(IPAddress.Parse(ipAddress)) ;
+        }
+
+        public void UDPing(string pingDest)  // pingDest 一定合法
+        {
+            if (IsAthernetSubnet(pingDest))
+            {
+                // 通过声卡发
+            }
+            else
+            {
+                // 通过网卡 (pcap) 发
+            }
         }
 
         public void getInterface()
@@ -75,7 +171,7 @@ namespace Native_Modem
             //Console.WriteLine("Please enter your MAC address (in decimal): ");
             var rand = new Random();
             byte address = (byte)rand.Next(255);  // 1.广播，这样只要保证两个 mac 不一样就行了 2. 建立 mac - ip 表
-           
+
             //while (!byte.TryParse(Console.ReadLine(), out address))
             //{
             //    Console.WriteLine("MAC address invalid. It should be an integer in [0, 255]. Please try again: ");
